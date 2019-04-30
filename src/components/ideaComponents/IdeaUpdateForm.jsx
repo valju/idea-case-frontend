f9d73e627cb1bc5bf49ed79cc07876df70aecdef
Tung Huynh <htqtung@users.noreply.github.com>
2019-05-01 00:29:04 +0300

    update features, working version

--- src/components/ideaComponents/IdeaUpdateForm.jsx
+++ src/components/ideaComponents/IdeaUpdateForm.jsx
@@ -31,88 +31,76 @@ export default class IdeaUpdateForm extends Component {
         })
     }
 
-    inputFieldValueChanged = (event) => {
-        this.setState(
-            {
-                ideaObject:
-                {
-                    ...this.state.ideaObject,
-                    [event.target.id]: event.target.value
-                }
-            });
+    inputFieldValueChanged = event => {
+        this.setState({
+            ideaObject: {
+                ...this.state.ideaObject,
+                [event.target.id]: event.target.value
+            }
+        });
     };
-}
 
-inputFieldValueChanged = event => {
-    this.setState({
-        ideaObject: {
-            ...this.state.ideaObject,
-            [event.target.id]: event.target.value
-        }
-    });
-};
+    toggleCheckboxValue = () => {
+        this.setState({
+            ideaObject: {
+                ...this.state.ideaObject,
+                readyForComments: !this.state.readyForComments,
+            }
+        });
+    }
 
-toggleCheckboxValue = () => {
-    this.setState({
-        ideaObject: {
-            ...this.state.ideaObject,
-            readyForComments: !this.state.readyForComments,
-        }
-    });
-}
+    handleSubmit = event => {
+        event.preventDefault();
 
-handleSubmit = event => {
-    event.preventDefault();
+        const ideaObject = this.state.ideaObject;
+        this.props.updateIdeaButtonClicked(ideaObject);
+    }
 
-    const ideaObject = this.state.ideaObject;
-    this.props.updateIdeaButtonClicked(ideaObject);
-}
+    render = () => {
+        return (
+            <form>
+                Name: <input
+                    id="name"
+                    type="text"
+                    onChange={this.inputFieldValueChanged}
+                    value={this.state.ideaObject.name} />
+                <br />
 
-render = () => {
-    return (
-        <form>
-            Name: <input
-                id="name"
-                type="text"
-                onChange={this.inputFieldValueChanged}
-                value={this.state.ideaObject.name} />
-            <br />
-
-            Description: <input
-                id="description"
-                type="text"
-                size="50"
-                value={this.state.ideaObject.description}
-                onChange={this.inputFieldValueChanged}
-                margin="normal"
-            />
-            <br />
-            Budget: <input
-                id="budget"
-                type="number"
-                value={this.state.ideaObject.budget}
-                onChange={this.inputFieldValueChanged}
-                margin="normal"
-            />
-            <br />
-            {/*Not working yet, cannot uncheck the value */}
-            Ready For Comment: <input
-                id="readyForComments"
-                type="checkbox"
-                checked={this.state.ideaObject.readyForComments}
-                value={this.state.ideaObject.readyForComments}
-                onChange={this.toggleCheckboxValue}
-                margin="normal"
-            />
-            <br />
-            People Needed: <input
-                id="peopleNeeded"
-                type="number"
-                value={this.state.ideaObject.peopleNeeded}
-                onChange={this.inputFieldValueChanged}
-                margin="normal"
-            />
-            {/* <br />
+                Description: <input
+                    id="description"
+                    type="text"
+                    size="50"
+                    value={this.state.ideaObject.description}
+                    onChange={this.inputFieldValueChanged}
+                    margin="normal"
+                />
+                <br />
+                Budget: <input
+                    id="budget"
+                    type="number"
+                    value={this.state.ideaObject.budget}
+                    onChange={this.inputFieldValueChanged}
+                    margin="normal"
+                />
+                <br />
+                {/*Not working yet, cannot uncheck the value */}
+                Ready For Comment: <input
+                    id="readyForComments"
+                    type="checkbox"
+                    checked={this.state.ideaObject.readyForComments}
+                    value={this.state.ideaObject.readyForComments}
+                    onChange={this.toggleCheckboxValue}
+                    margin="normal"
+                />
+                <br />
+                People Needed: <input
+                    id="peopleNeeded"
+                    type="number"
+                    value={this.state.ideaObject.peopleNeeded}
+                    onChange={this.inputFieldValueChanged}
+                    margin="normal"
+                />
+                {/* <br />
                 Category ID: <input
                     id="categoryId"
                     type="number"
@@ -120,25 +108,25 @@ render = () => {
                     onChange={this.inputFieldValueChanged}
                     margin="normal"
                 /> */}
-            <br />
-            {/*Working but the field always show the first option, not the current value*/}
-            Category:
+                <br />
+                {/*Working but the field always show the first option, not the current value*/}
+                Category:
                 <select id="categoryId" onChange={this.inputFieldValueChanged}>
-                {
-                    this.props.categories.map((item) =>
-                        <option key={item.id} value={item.id}>{item.name}</option>
-                    )
+                    {
+                        this.props.categories.map((item) =>
+                            <option key={item.id} value={item.id}>{item.name}</option>
+                        )
 
-                }
-            </select>
+                    }
+                </select>
 
-            <br />
-            <button type="button" onClick={this.handleSubmit}>
-                UPDATE IDEA
+                <br />
+                <button type="button" onClick={this.handleSubmit}>
+                    UPDATE IDEA
                 </button>
 
-        </form>
-    );
-};
-
-// export default withRouter(IdeaUpdateForm);
+            </form>
+        );
+    };
+}
+// export default withRouter(IdeaUpdateForm)
