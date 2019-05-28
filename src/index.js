acaa23fb4f01b9a3a6a5349e2468d4f56a50609c
Josiah <Josiahsingh1996@gmail.com>
2019-05-28 20:41:53 +0300

    added mui-theme and updated action types for auth

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(
-    rootReducer(),
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
- );
+export const store = createStore(
+  rootReducer(),
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,  
-    document.getElementById('root')
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
